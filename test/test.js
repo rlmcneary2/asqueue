096fb65b514f019d50b97fc65cec153145fba472
Rich McNeary <rlmcneary2@gmail.com>
2017-07-30 11:55:20 -0400

    Fix test by adding async.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const asqueue = require("../dist/index").default;
+const asqueue = require("../lib/asqueue").default;
 const expect = require("chai").expect;
 
 debugger;
 
 describe("add a task", () => {
 
-    it("should return a promise", () => {
+    it("should return a promise", async () => {
         let success = true;
         await asqueue();
         expect(success).to.equal(true, "returned a promise");
